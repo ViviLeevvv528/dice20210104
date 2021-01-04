bf18f16bb816f34726f18e628273abaa90869ebd
vivi <ashin-jo@yahoo.com.tw>
2021-01-04 19:28:16 +0800

    Initial Commit

---
+++ dice20210104/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice20210104
+//
+//  Created by Vivi Lee on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
