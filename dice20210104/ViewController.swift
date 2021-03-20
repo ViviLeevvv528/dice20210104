f001c9433a0d7aa3b482934605e34ef890d32037
vivi <ashin-jo@yahoo.com.tw>
2021-03-21 00:03:46 +0800

    the first

--- dice20210104/ViewController.swift
+++ dice20210104/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dicePCImageview: [UIImageView]!
+    
+    @IBOutlet var diceYouImageview: [UIImageView]!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    var sumPC = 0
+    var sumYou = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultImage.isHidden = true
     }
 
+    @IBAction func actionBtn(_ sender: UIButton) {
+        for imageViewA in dicePCImageview {
+        let numberPC = Int.random(in: 1...6)
+        imageViewA.image = UIImage(systemName: "dice.\(numberPC)")
+        }
+        for imageViewB in diceYouImageview {
+        let numberYou = Int.random(in: 1...6)
+        imageViewB.image = UIImage(systemName: "dice.\(numberYou)")
+        }
+    }
 
+//
+//        resultImage.image = UIImage(named: "win")
+//
+//        resultImage.image = UIImage(named: "lose")
+        
+
+
+    
 }
 
